0c3961b8925d682038ab252dc2bd0715d9c814c0
renato.lobo <renato.lobo@totvs.com.br>
2024-06-13 19:49:11 -0300

    nova versão 9

--- Apps/Exemplo002/Program.cs
+++ Apps/Exemplo002/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Exemplo002 versoa 1.0.0.7");
+Console.WriteLine("Hello, World! Exemplo002 versoa 1.0.0.9");
 Console.ReadLine();
